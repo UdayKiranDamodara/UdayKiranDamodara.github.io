aaea60c23d97a929617bd16d91baa1546b0a9349
UdayKiranDamodara <udaykirandamodara@gmail.com>
2020-06-30 20:20:14 +0530

    adding react project

---
+++ friends/precache-manifest.33a655602cb3f4a565a1ae6373c897fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb31d661c55139c725d5242f1fd4e41",
+    "url": "./index.html"
+  },
+  {
+    "revision": "401f6c128aff53f9e2ca",
+    "url": "./static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "448d2480b04452025cb9",
+    "url": "./static/css/main.21a05347.chunk.css"
+  },
+  {
+    "revision": "401f6c128aff53f9e2ca",
+    "url": "./static/js/2.912d5547.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.912d5547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "448d2480b04452025cb9",
+    "url": "./static/js/main.5d47b4df.chunk.js"
+  },
+  {
+    "revision": "655bad39d5d36fe8103f",
+    "url": "./static/js/runtime-main.4db928db.js"
+  }
+]);
