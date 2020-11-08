8b39c09adf6985312a5d6a02f9acd655c6b366f4
UdayKiranDamodara <udaykirandamodara@gmail.com>
2020-11-08 10:01:13 +0530

    added League API work

---
+++ League/precache-manifest.3f34cb3f5437e15b62943e2c1dc8a663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a62761dd6a48176486388aedbd0517",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ca6045264286df8674e",
+    "url": "./static/css/main.9480e152.chunk.css"
+  },
+  {
+    "revision": "0c3326403aade99964c9",
+    "url": "./static/js/2.6385c032.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.6385c032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca6045264286df8674e",
+    "url": "./static/js/main.c364f72b.chunk.js"
+  },
+  {
+    "revision": "a98e3dcb6943896c59bb",
+    "url": "./static/js/runtime-main.e39d38a1.js"
+  }
+]);
